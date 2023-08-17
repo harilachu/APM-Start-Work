30abd94312f78757e3405071bf03ffe9bf30d454
Hariprasad J <hariprasad.becse@gmail.com>
2023-08-17 11:10:12 +0530

    Routing and Route gaurd

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from '../Interfaces/product';
+import { ProductService } from '../Services/product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   templateUrl: './product-details.component.html',
@@ -7,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
 export class ProductDetailsComponent implements OnInit {
 
   pageTitle: string = "Product Details";
+  products: IProduct[] |undefined;
+  product: IProduct |undefined;
+  private productObservable: Subscription | undefined;
+  errorMessage: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private productService: ProductService, private router: Router) {
+
+  }
 
   ngOnInit(): void {
+   const id = Number(this.route.snapshot.paramMap.get('id'));
+   this.productObservable = this.productService.getProducts().subscribe({
+    next: products =>
+    {
+      this.products = products;
+      this.product = this.products.find(x=>x.productId === id);
+    },
+    error: err=> this.errorMessage = err
+    });
+  }
+
+  OnBack(): void{
+    this.router.navigate(["/products"]);
   }
 
 }
